caf86402e5c022d3d3a2ffcf761c4dd4b46516d4
Marshall Buck <dev@marshallbuck.com>
2022-06-30 16:27:43 -0500

    starting and restarting functionality works

--- memory-game.js
+++ memory-game.js
@@ -3,62 +3,21 @@
 /** Memory game: find matching pairs of cards and flip both of them. */
 
 const FOUND_MATCH_WAIT_MSECS = 1000;
-const PAINT_TIME = 20;
+const PAINT_TIME = 60;
 
 
 
 let imgSrcs = [];
 let imgSrcShuffled;
 
-
-
-
 let firstCardFlipped;
 let activeCards = 0;
-let gameState = { inProgress: false, fetching: false };
+
 const form = document.querySelector('#start');
 
 
 
-form.addEventListener('submit', async (e) => {
-  e.preventDefault();
-
-  const num = form.elements.num.value;
-  if (isNaN(num) || num < 1 || num > 50) {
-    window.alert('Please enter a number between 1 and 50');
-    return;
-  }
-
-  const page = await getRandomPageNumber();
-
-  const apiLinks = await fetchApiLinks(num, page);
-
-  const srcs = await fetchImageIds(apiLinks);
-  imgSrcs = [...srcs, ...srcs];
-
-
-
-
-  if (gameState.inProgress === false) {
-    imgSrcShuffled = shuffle(imgSrcs);
-    createCards(imgSrcShuffled);
-    gameState.inProgress = true;
-    form.lastElementChild.innerText = "Restart";
-    form.reset();
-
-
-  } else {
-    resetBoard();
-    setTimeout(() => {
-      imgSrcShuffled = shuffle(imgSrcs);
-      createCards(imgSrcShuffled);
-    }, 800);
-
-  }
-
-
-
-});
+form.addEventListener('submit', handelFormSubmission);
 /** Shuffle array items in-place and return shuffled array. */
 
 function shuffle(items) {
@@ -85,6 +44,8 @@ function shuffle(items) {
  */
 
 function createCards(srcs) {
+
+
   const gameBoard = document.getElementById("game");
   let count = 0;
   for (let i = 0; i < srcs.length; i++) {
@@ -103,15 +64,26 @@ function createCards(srcs) {
   gameBoard.style.opacity = '1';
 }
 
+function deleteCards() {
+  const cards = document.querySelectorAll('.card');
+  let count = 0;
+  for (let card of cards) {
+    count++;
+    setTimeout(() => {
+      card.remove();
+    }, PAINT_TIME * count);
+  }
+  imgSrcs = [];
+  imgSrcShuffled = [];
+
+}
+
 /** Flip a card face-up.
  *make sure only one other card is 'on'
  */
 
 function flipCard(card) {
   const index = card.id;
-  // const p = document.createElement('p');
-  // p.innerText = imgSrcShuffled[index];
-  // card.append(p);
   const img = document.createElement('img');
   img.src = imgSrcShuffled[index];
   card.append(img);
@@ -139,19 +111,17 @@ function unFlipCard(card) {
 /** Handle clicking on a card: this could be first-card or second-card. */
 
 function handleCardClick(evt) {
-  if (gameState.inProgress === false) return;
   const card = evt.target;
   if (!isCardOff(card) || activeCards === 2) {
+    console.log('');
     return;
   }
   else {
     flipCard(card);
-
     //  is there a currentCard ie. is this the second card flipped
     if (activeCards === 2) {
-
+      // a match occurred
       if (imgSrcShuffled[firstCardFlipped.id] === imgSrcShuffled[card.id]) {
-        // a match occurred
         card.classList.add('match');
         card.classList.toggle('anim');
         const first = document.getElementById(firstCardFlipped.id);
@@ -161,12 +131,14 @@ function handleCardClick(evt) {
 
         if (isEndOfGame()) {
           setTimeout(() => {
-            form.innerText = 'Start';
-            gameState.inProgress = false;
+            console.log('end of game');
+            activeCards = 0;
+            firstCardFlipped = undefined;
           }, 500);
 
         } else { firstCardFlipped = undefined; activeCards = 0; }
       } else {
+
         setTimeout(() => {
           unFlipCard(card);
           unFlipCard(firstCardFlipped);
@@ -189,7 +161,38 @@ function handleCardClick(evt) {
 }
 
 
+/** Handle form submission */
+async function handelFormSubmission(e) {
+  e.preventDefault();
+  deleteCards();
+  const num = form.elements.num.value;
+  if (isNaN(num) || num < 1 || num > 50) {
+    window.alert('Please enter a number between 1 and 50');
+    return;
+  }
+  showLoader('Please wait while images are retrieved');
+  const page = await getRandomPageNumber();
 
+  const apiLinks = await fetchApiLinks(num, page);
+
+  const srcs = await fetchImageIds(apiLinks);
+  imgSrcs = [...srcs, ...srcs];
+
+
+
+
+  imgSrcShuffled = shuffle(imgSrcs);
+  createCards(imgSrcShuffled);
+
+  form.lastElementChild.innerText = "Restart";
+  form.reset();
+
+
+
+
+
+
+}
 
 function isCardOff(card) {
   if (card.classList.contains('off')) return true;
@@ -200,29 +203,20 @@ function isCardOff(card) {
 function isEndOfGame() {
 
   for (let card of document.querySelectorAll('.card')) {
-    if (card.classList.contains('off')) return false;
+    if (isCardOff(card)) return false;
   }
   return true;
 }
 
-function resetBoard() {
-  const cards = document.querySelectorAll('.card');
-  let count = 0;
-  for (let card of cards) {
-    count++;
-    setTimeout(() => {
-      card.remove();
-    }, PAINT_TIME * count);
-  }
 
-}
-// returns a randompage number to retrieve images from
+// returns a random page number to retrieve images from
 async function getRandomPageNumber() {
   return Math.floor(Math.random() * (90 - 1) + 1);
 }
 
 async function fetchApiLinks(num, page) {
-  gameState.fetching = true;
+
+
 
   try {
     const response = await fetch(`https://api.artic.edu/api/v1/artworks/search?query[term][is_public_domain]=true&fields=api_link,pagination&limit=${num}&page=${page}`);
@@ -234,32 +228,40 @@ async function fetchApiLinks(num, page) {
     }
     return apiLinks;
   } catch (error) {
-    gameState.fetching = false;
+
     console.log(error);
   }
 
 
 }
 async function fetchImageIds(array) {
-  let outPut = [];
+  let imgSrcs = [];
   for (const link of array) {
     try {
       const response = await fetch(`${link}/?fields=image_id`);
       const json = await response.json();
       const result = await json;
 
-      outPut.push(`https://www.artic.edu/iiif/2/${result.data.image_id}/full/200,/0/default.jpg`);
+      imgSrcs.push(`https://www.artic.edu/iiif/2/${result.data.image_id}/full/200,/0/default.jpg`);
     } catch (error) {
       console.log(error);
-      gameState.fetching = false;
+
     }
-
-
   }
-  gameState.fetching = false;
-  return outPut;
+  removeLoader();
+  return imgSrcs;
 }
 
+function showLoader(message) {
+  const container = document.querySelector('.container');
+  const loading = document.createElement('dialogue');
+  loading.classList = 'loading';
+  loading.innerText = message;
+  container.append(loading);
+}
+function removeLoader() {
+  document.querySelector('.loading').remove();
+}
 
 
 // }
